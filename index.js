0f3e01b4f06340a17a9b9fd7b1a625e4eb7eae8f
PeterNgTr <peter.nguyentr@gmail.com>
2019-10-28 10:50:23 +0100

    introduce attachment to test results

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const event = require('codeceptjs').event;
-const Testrail = require('testrail-api');
+const axios = require('axios');
+var FormData = require('form-data');
+var fs = require('fs');
+const Container = require('codeceptjs').container;
+const helpers = Container.helpers();
+let helper;
+
+const supportedHelpers = [
+	'WebDriverIO',
+	'WebDriver',
+	'Protractor',
+	'Appium',
+	'Nightmare',
+	'Puppeteer',
+];
+
+for (const helperName of supportedHelpers) {
+	if (Object.keys(helpers).indexOf(helperName) > -1) {
+		helper = helpers[helperName];
+	}
+}
 
 const defaultConfig = {
 	host: '',
@@ -28,28 +48,112 @@ function getToday() {
 	return `${dd}/${mm}/${yyyy}`;
 }
 
+class TestRail {
+	constructor(defaultConfig) {
+		this.host = defaultConfig.host;
+		this.user = defaultConfig.user;
+		this.password = defaultConfig.password;
+
+		this.uri = '/index.php?/api/v2/';
+
+		axios.defaults.baseURL = this.host + this.uri;
+	}
+
+	async getSuites(projectId) {
+		let res = await axios({
+			method: 'get',
+			url: 'get_suites/' + projectId,
+			auth: {
+				username: this.user,
+				password: this.password
+			},
+			headers: {
+				'content-type': 'application/json'
+			}
+		});
+		return res.data;
+
+	}
+
+	async addRun(projectId, data) {
+		let res = await axios({
+			method: 'post',
+			url: 'add_run/' + projectId,
+			data,
+			auth: {
+				username: this.user,
+				password: this.password
+			}
+		});
+		return res.data;
+	}
+
+	async updateRun(runId, data) {
+		let res = await axios({
+			method: 'post',
+			url: 'update_run/' + runId,
+			data,
+			auth: {
+				username: this.user,
+				password: this.password
+			}
+		});
+		return res.data;
+	}
+
+	async addResultForCase(runId, caseId, data) {
+		let res = await axios({
+			method: 'post',
+			url: 'add_result_for_case/' + runId + '/' + caseId,
+			data,
+			auth: {
+				username: this.user,
+				password: this.password
+			}
+		});
+		return res.data;
+	}
+
+	async addAttachmentToResult(resultId, imageFile) {
+		var form = new FormData();
+		form.append('attachment', fs.createReadStream(global.output_dir + '/' + imageFile)); 
+
+		axios({
+			method: 'post',
+			data: form,
+			url: 'add_attachment_to_result/' + resultId,
+			auth: {
+				username: this.user,
+				password: this.password
+			},
+			headers: form.getHeaders()
+		}).catch(err => {
+			throw Error(`Cannot attach file due to ${err}`);
+		});
+	}
+}
+
 module.exports = (config) => {
 	config = Object.assign(defaultConfig, config);
 
 	if (config.host === '' || config.user === '' || config.password === '') throw new Error('Please provide proper Testrail host or credentials');
 	if (!config.projectId) throw new Error('Please provide project id');
 
-	const testrail = new Testrail(config);
+	const testrail = new TestRail(config);
 
 	let suiteId;
 	let runName;
 	let runId;
 	let failedTests = [];
 	let errors = {};
+	let attachments = {};
 	let passedTests = [];
 
 	runName = config.runName ? config.runName : `This is a new test run on ${getToday()}`;
 
-	function _addTestRun(projectId, suiteId, runName) {
-		testrail.addRun(projectId, { suite_id: suiteId, name: runName, include_all: false }, (err, response, run) => {
-			if (err) throw new Error(`Something is wrong while adding new run with name ${runName}. Please check ${JSON.stringify(err)}`);
-			runId = run.id;
-		});
+	async function _addTestRun(projectId, suiteId, runName) {
+		let res = await testrail.addRun(projectId, { suite_id: suiteId, name: runName, include_all: false });
+		return res;
 	}
 
 	async function _updateTestRun(runId, ids) {
@@ -57,20 +161,27 @@ module.exports = (config) => {
 	}
 
 	if (config.suiteId === undefined || config.suiteId === null) {
-		testrail.getSuites(config.projectId, function (err, response, suites) {
-			if (err) throw new Error(`Something is wrong while getting suites of project ID: ${config.projectId}. Please check ${JSON.stringify(err)}`);
-			suiteId = suites[0].id;
-			_addTestRun(config.projectId, suiteId, runName);
+		testrail.getSuites(config.projectId).then(res => {
+			const suiteId = res[0].id;
+			_addTestRun(config.projectId, suiteId, runName).then(res => {
+				runId = res.id;
+			});
 		});
 	} else {
 		suiteId = config.suiteId;
-		_addTestRun(config.projectId, suiteId, runName);
+		_addTestRun(config.projectId, suiteId, runName).then(res => {
+			runId = res.id;
+		});
 	}
 
-	event.dispatcher.on(event.test.failed, (test, err) => {
-		test.tags.forEach(tag => {
+	event.dispatcher.on(event.test.failed, async (test, err) => {
+		test.tags.forEach(async (tag) => {
+			const uuid = Math.floor(new Date().getTime() / 1000);
+			const fileName = `${uuid}.failed.png`;
+			helper.saveScreenshot(fileName);
 			failedTests.push(tag.split('@C')[1]);
 			errors[tag.split('@C')[1]] = err;
+			attachments[tag.split('@C')[1]] = fileName;
 		});
 	});
 
@@ -100,8 +211,15 @@ module.exports = (config) => {
 				errorString = errors[id];
 			}
 			let failedCase = { status_id: 5, comment: `This test is failed due to **${errorString}**` };
-			testrail.addResultForCase(runId, id, failedCase, (err) => {
-				if (err) throw new Error(`Something is wrong while adding result for a test case ${id}. Please check ${JSON.stringify(err)}`);
+			testrail.addResultForCase(runId, id, failedCase).then(res => {
+				let resultId = res.id;
+				testrail.addAttachmentToResult(resultId, attachments[id]);
+				try {
+					fs.unlinkSync(global.output_dir + '/' + attachments[id]);
+					//file removed
+				} catch(err) {
+					throw Error(`Cannot remove file due to ${err}`);
+				}
 			});
 		});
 	});
