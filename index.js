aa0e6a913e2be01e764f071ba98f2dc83c345b4e
RuslanAisin <49075028+RuslanAisin@users.noreply.github.com>
2020-01-24 19:39:58 +0300

    add time stamp in testrun name

--- index.js
+++ index.js
@@ -37,15 +37,24 @@ function getToday() {
 	const today = new Date();
 	let dd = today.getDate();
 	let mm = today.getMonth() + 1; // January is 0!
-
 	const yyyy = today.getFullYear();
+
+	let hour = today.getHours();
+    let minute = today.getMinutes();
+
+
+	
 	if (dd < 10) {
 		dd = `0${dd}`;
 	}
 	if (mm < 10) {
 		mm = `0${mm}`;
 	}
-	return `${dd}/${mm}/${yyyy}`;
+	if (minute < 10) {
+		minute = `0${minute}`;
+	}
+
+	return `√ ${dd}/${mm}/${yyyy} ${hour}:${minute}`; 
 }
 
 class TestRail {
